3bcfbfc195352bdc677952b759d91965296b272b
Guilherme Ligeski Saldanha <112131340+GuiLigeskee@users.noreply.github.com>
2024-03-21 10:52:30 -0300

    atualizações dia 21/03/24
    --Feita a seção de "conhecimentos" na página

--- portifolio/src/components/Acquirements/Acquirements.jsx
+++ portifolio/src/components/Acquirements/Acquirements.jsx
@@ -7,6 +7,16 @@ const Acquirements = () => {
     <div className="acquirements">
       <h1 className="title">Conhecimentos</h1>
       <Carousel />
+      <p className="text">
+        Tenho conhecimento aprofundado em <span>JavaScript</span>,{" "}
+        <span>React</span>, <span>Redux</span> e <span>Node.js</span>, criando
+        aplicações web dinâmicas e eficientes. Possuo amplo conhecimento em
+        banco de dados <span>MongoDB</span> para armazenamento de dados flexível
+        e escalável. Além disso, estou familiarizado com <span>Git</span> e{" "}
+        <span>GitHub</span> para controle de versão e colaboração em equipe.
+        Tenho habilidades básicas em <span>Python</span>, <span>C</span> e{" "}
+        <span>C#</span>, adquiridas durante minha formação acadêmica.
+      </p>
     </div>
   );
 };
