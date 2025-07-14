12cf3598511843a4e972ee7a4464868f362aa013
Guilherme Saldanha <guiligeskee@gmail.com>
2025-07-14 12:26:56 -0300

    update 2025

--- portifolio/src/components/AboutMe/AboutMe.jsx
+++ portifolio/src/components/AboutMe/AboutMe.jsx
@@ -1,6 +1,6 @@
 import "./AboutMe.css";
 import ScrollIndicator from "../ScrollIndicator/ScrollIndicator";
-import { FaGraduationCap, FaBriefcase, FaLaptopCode } from "react-icons/fa";
+import { FaGraduationCap, FaBriefcase } from "react-icons/fa";
 
 const AboutMe = () => {
   return (
@@ -8,11 +8,11 @@ const AboutMe = () => {
       <h1 className="title">Sobre Mim</h1>
       <p className="text">
         Olá, sou <span>Guilherme Ligeski Saldanha</span>, um{" "}
-        <span>desenvolvedor fullstack</span> de <span>20 anos</span>.
-        Atualmente, estou no 6º período de <span>Engenharia de Software</span>
-        na UniBrasil e tenho experiência no desenvolvimento de{" "}
-        <span>APIs e sistemas escaláveis</span>, criando soluções personalizadas
-        para empresas.
+        <span>desenvolvedor fullstack</span> de <span>20 anos</span>. Estudante
+        de <span>Engenharia de Software</span>, tenho experiência na criação de{" "}
+        <span>APIs e sistemas escaláveis</span>. Atualmente, atuo como{" "}
+        <span>Desenvolvedor Full-Stack e Coordenador de Projetos</span> na Pew's
+        Cavern.
       </p>
 
       <div className="timeline">
@@ -22,6 +22,18 @@ const AboutMe = () => {
           <div className="timeline-content">
             <h3>Engenharia de Software</h3>
             <p>UniBrasil (2022 - 2025)</p>
+          </div>
+        </div>
+
+        <div className="timeline-item">
+          <FaBriefcase className="icon" />
+          <div className="timeline-content">
+            <h3>Dev. Full-Stack e Coordenador de Projetos</h3>
+            <p>Pew's Cavern (2025 - Presente)</p>
+            <p>
+              Liderança de equipe com SCRUM e desenvolvimento full-stack com
+              React, Node.js e n8n.
+            </p>
           </div>
         </div>
 
